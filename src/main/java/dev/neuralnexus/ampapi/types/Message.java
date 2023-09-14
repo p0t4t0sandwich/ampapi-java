83326bba914570f51838d19f5f889e67c001edc6
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2023-09-14 00:57:38 -0600

    Slight type refactor

---
+++ src/main/java/dev/neuralnexus/ampapi/types/Message.java
@@ -0,0 +1,32 @@
+package dev.neuralnexus.ampapi.types;
+
+import java.util.UUID;
+
+/**
+ * Message type for API.Core.GetUpdates status messages (along with WS keep alive)
+ * @author p0t4t0sandwich
+ * @see Updates
+ * @see dev.neuralnexus.ampapi.apimodules.Core#GetUpdates()
+ */
+public class Message {
+    /**
+     * Id - The ID of the message
+     * Expired - Whether the message has expired
+     * Source - The source of the message
+     * Message - The message
+     * AgeMinutes - The age of the message in minutes
+     */
+    public UUID Id;
+    public boolean Expired;
+    public String Source;
+    public String Message;
+    public double AgeMinutes;
+
+    /**
+     * toString
+     * @return A string representation of the object
+     */
+    public String toString() {
+        return "Message{Id=" + this.Id + ", Expired=" + this.Expired + ", Source=" + this.Source + ", Message=" + this.Message + ", AgeMinutes=" + this.AgeMinutes + "}";
+    }
+}
--- src/main/java/dev/neuralnexus/ampapi/types/Updates.java
+++ src/main/java/dev/neuralnexus/ampapi/types/Updates.java
@@ -28,33 +28,4 @@ public class Updates {
     public String toString() {
         return "Updates{Status=" + this.Status + ", ConsoleEntries=" + Arrays.toString(this.ConsoleEntries) + ", Messages=" + Arrays.toString(this.Messages) + ", Tasks=" + Arrays.toString(this.Tasks) + ", Ports=" + Arrays.toString(this.Ports) + "}";
     }
-
-    /**
-     * Message type for API.Core.Updates status messages
-     * @author p0t4t0sandwich
-     * @see Updates
-     * @see dev.neuralnexus.ampapi.apimodules.Core#GetUpdates()
-     */
-    public static class Message {
-        /**
-         * Id - The ID of the message
-         * Expired - Whether the message has expired
-         * Source - The source of the message
-         * Message - The message
-         * AgeMinutes - The age of the message in minutes
-         */
-        public String Id;
-        public boolean Expired;
-        public String Source;
-        public String Message;
-        public double AgeMinutes;
-
-        /**
-         * toString
-         * @return A string representation of the object
-         */
-        public String toString() {
-            return "Message{Id=" + this.Id + ", Expired=" + this.Expired + ", Source=" + this.Source + ", Message=" + this.Message + ", AgeMinutes=" + this.AgeMinutes + "}";
-        }
-    }
 }
