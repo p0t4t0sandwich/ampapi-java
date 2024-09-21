8bda65e443783a9ed583fe9a73b50b2933dd2b6a
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2024-09-21 00:35:17 -0600

    Fixed licence header

--- src/main/java/dev/neuralnexus/ampapi/exceptions/APILoginException.java
+++ src/main/java/dev/neuralnexus/ampapi/exceptions/APILoginException.java
@@ -1,6 +1,6 @@
 /**
  * Copyright (c) 2024 Dylan Sperrer - dylan@sperrer.ca
- * The project is Licensed under <a href="https://github.com/p0t4t0sandwich/TaterLib/blob/dev/LICENSE-API">MIT</a>
+ * This project is Licensed under <a href="https://github.com/p0t4t0sandwich/ampapi-java/blob/main/LICENSE">MIT</a>
  */
 package dev.neuralnexus.ampapi.exceptions;
 
